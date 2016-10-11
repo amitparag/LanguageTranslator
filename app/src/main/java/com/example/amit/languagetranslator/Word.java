b809a36e175c89382bc1236be2e1b5f6baaac134
Amit Parag <amit.parag006@gmail.com>
2016-10-12 00:03:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amit/languagetranslator/Word.java
@@ -0,0 +1,25 @@
+package com.example.amit.languagetranslator;
+public class Word {
+
+
+    private String mDefaultTranslation;
+
+    private String mMiwokTranslation;
+
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+}
